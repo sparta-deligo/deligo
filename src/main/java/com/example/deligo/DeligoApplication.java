48fc9c00793d24600289fb7547f0f92baa413e8c
subbni <slfkak@naver.com>
2025-02-28 20:04:54 +0900

    build: 프로젝트 생성

---
+++ src/main/java/com/example/deligo/DeligoApplication.java
@@ -0,0 +1,13 @@
+package com.example.deligo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeligoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeligoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deligo/DeligoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deligo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeligoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
