5ef1ffb9a49ffdf7ca6565d7af07ad10f2d978e7
subbni <slfkak@naver.com>
2025-02-28 22:29:01 +0900

    feat: 공통 Exception 모듈 구현 (#3)

---
+++ src/main/java/com/example/deligo/common/exception/CustomException.java
@@ -0,0 +1,29 @@
+package com.example.deligo.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ExceptionType exceptionType;
+
+    public CustomException(ExceptionType exceptionType) {
+        super(exceptionType.getMessage());
+        this.exceptionType = exceptionType;
+    }
+
+    public CustomException(ExceptionType exceptionType, String message) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return exceptionType.getHttpStatus();
+    }
+
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
+
