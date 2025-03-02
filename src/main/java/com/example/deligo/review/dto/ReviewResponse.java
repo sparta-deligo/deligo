c2fe0e0d944966f744d3f2450b016c73cb9d18b1
J <kj5592305@gmail.com>
2025-03-02 17:34:28 +0900

    ReviewResponse add

--- src/main/java/com/example/deligo/review/dto/ReviewResponse.java
+++ src/main/java/com/example/deligo/review/dto/ReviewResponse.java
@@ -1,7 +1,24 @@
 package com.example.deligo.review.dto;
 
+import com.example.deligo.review.entity.OwnerComment;
+import com.example.deligo.review.entity.Review;
+
 public class ReviewResponse {
     private final Long id;
     private final Long userId;
+    private final Long orderId;
+    private final int rating;
+    private final String content;
+    private final String ownerComment;
 
+    public ReviewResponse(Review review) {
+        this.id = review.getId();
+        this.userId = review.getUser().getId();
+        this.orderId = review.getOrder().getId();
+        this.rating = review.getRating();
+        this.content = review.getContent();
+        this.ownerComment = review.getOwnerComment()
+                .map(OwnerComment::getContent)
+                .orElse(null);
+    }
 }
