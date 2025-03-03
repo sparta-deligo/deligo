2c1391f9b9de6581fb14791aa7cd8dc8b22c7f68
J <kj5592305@gmail.com>
2025-03-03 13:17:40 +0900

    response edit

--- src/main/java/com/example/deligo/review/dto/ReviewResponse.java
+++ src/main/java/com/example/deligo/review/dto/ReviewResponse.java
@@ -3,27 +3,28 @@ package com.example.deligo.review.dto;
 import com.example.deligo.common.entity.BaseTimeEntity;
 import com.example.deligo.review.entity.OwnerComment;
 import com.example.deligo.review.entity.Review;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
+@Getter
 public class ReviewResponse extends BaseTimeEntity {
-    private Long id;
-    private Long userId;
-    private Long orderId;
-    private int rating;
-    private String content;
-    private LocalDateTime deletedAt;
-    private String ownerComment;
+    private final Long id;
+    private final Long userId;
+    private final Long orderId;
+    private final int rating;
+    private final String content;
+    private final LocalDateTime deletedAt;
+    private final String ownerComment;
 
-    public ReviewResponse(Review review) {
+    private ReviewResponse(Review review) {
         this.id = review.getId();
         this.userId = review.getUser().getId();
         this.orderId = review.getOrder().getId();
         this.rating = review.getRating();
         this.content = review.getContent();
         this.deletedAt = review.getDeletedAt();
-        this.ownerComment = Optional.ofNullable(review.getOwnerComment())
+        this.ownerComment = review.getOwnerComment()
                 .map(OwnerComment::getContent)
                 .orElse(null);
     }
