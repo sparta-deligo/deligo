a8059b423ebcfa1c66c6680220a55507e3fc6086
J <kj5592305@gmail.com>
2025-03-03 13:24:40 +0900

    OwenrCommentRepository edit

--- src/main/java/com/example/deligo/review/repository/OwnerCommentRepository.java
+++ src/main/java/com/example/deligo/review/repository/OwnerCommentRepository.java
@@ -3,5 +3,10 @@ package com.example.deligo.review.repository;
 import com.example.deligo.review.entity.OwnerComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OwnerCommentRepository extends JpaRepository<OwnerComment, Long> {
+    Optional<OwnerComment> findByReviewId(Long reviewId);
+    Optional<OwnerComment> findByOwnerId(Long ownerId);
+    boolean existsByReviewId(Long reviewId);
 }
