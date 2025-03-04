79c7f60b96f0a26ce1ac7c8c3bb8f4b0c315c497
subbni <slfkak@naver.com>
2025-03-04 11:15:38 +0900

    feat: 가게별 Menu 조회 메서드 구현 (#8)

--- src/main/java/com/example/deligo/menu/repository/MenuRepository.java
+++ src/main/java/com/example/deligo/menu/repository/MenuRepository.java
@@ -1,9 +1,17 @@
 package com.example.deligo.menu.repository;
 
 import com.example.deligo.menu.entity.Menu;
+import com.example.deligo.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    @Query("select m from Menu m where m.store = :store and m.status != 'DELETED'")
+    List<Menu> findAllByStore(@Param("store") Store store);
 }
