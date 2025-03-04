2a1ec62509a2811e6689386a2e30ceaf2e42ca1f
myeongje <tiqmtiqm6541@gmail.com>
2025-03-04 15:01:51 +0900

    fix:리퀘스트,리스폰스 수정

--- src/main/java/com/example/deligo/store/dto/request/StoreUpdateRequestDto.java
+++ src/main/java/com/example/deligo/store/dto/request/StoreUpdateRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.deligo.store.dto.request;
 
-import com.example.deligo.common.enums.StoreStatus;
+import com.example.deligo.store.entity.StoreCategory;
+import com.example.deligo.store.entity.StoreStatus;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -9,19 +10,21 @@ import java.time.LocalTime;
 @Getter
 public class StoreUpdateRequestDto {
 
+    private final Long ownerId;
     private final String name;
+    private final StoreCategory storeCategory;
     private final LocalTime openTime;
     private final LocalTime closeTime;
     private final int minOrderAmount;
-    private final String category;
     private final StoreStatus status;
 
-    public StoreUpdateRequestDto(String name, LocalTime openTime, LocalTime closeTime, int minOrderAmount, String category, StoreStatus status) {
+    public StoreUpdateRequestDto(Long ownerId, String name, StoreCategory storeCategory, LocalTime openTime, LocalTime closeTime, int minOrderAmount, StoreStatus status) {
+        this.ownerId = ownerId;
         this.name = name;
+        this.storeCategory = storeCategory;
         this.openTime = openTime;
         this.closeTime = closeTime;
         this.minOrderAmount = minOrderAmount;
-        this.category = category;
         this.status = status;
     }
 }
