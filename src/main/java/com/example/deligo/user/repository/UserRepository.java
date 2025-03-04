a379f29314466c2659783cf44665086bd5166b4c
J <kj5592305@gmail.com>
2025-03-04 16:43:27 +0900

    Jwt Secret Key 적용

--- src/main/java/com/example/deligo/user/repository/UserRepository.java
+++ src/main/java/com/example/deligo/user/repository/UserRepository.java
@@ -8,8 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-<<<<<<< HEAD
     Optional<User> findByEmail(String email);
-=======
->>>>>>> dev
 }
