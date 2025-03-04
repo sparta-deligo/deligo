af2e830ff92f666d774596abba52b1fd8c56aa74
myeongje <tiqmtiqm6541@gmail.com>
2025-03-04 15:00:06 +0900

    feat:업데이트 추가

--- src/main/java/com/example/deligo/store/entity/Store.java
+++ src/main/java/com/example/deligo/store/entity/Store.java
@@ -1,6 +1,6 @@
 package com.example.deligo.store.entity;
 import com.example.deligo.common.entity.BaseDeletableEntity;
-import com.example.deligo.common.enums.StoreStatus;
+import com.example.deligo.store.dto.request.StoreUpdateRequestDto;
 import com.example.deligo.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -45,6 +45,15 @@ public class Store extends BaseDeletableEntity {
             LocalTime closeTime, int minOrderAmount, StoreStatus status
     ) {
         this.owner = user;
+        this.name = name;
+        this.category = category;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.minOrderAmount = minOrderAmount;
+        this.status = status;
+    }
+
+    public void Update(StoreUpdateRequestDto dto) {
         this.name = name;
         this.category = category;
         this.openTime = openTime;
