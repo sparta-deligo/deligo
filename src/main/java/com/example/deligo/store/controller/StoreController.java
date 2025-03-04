94972cd80ebc8e5ec95fe8f2f8e0e3d7fa195461
myeongje <tiqmtiqm6541@gmail.com>
2025-03-04 15:02:27 +0900

    fix:컨트롤러,서비스 수정

--- src/main/java/com/example/deligo/store/controller/StoreController.java
+++ src/main/java/com/example/deligo/store/controller/StoreController.java
@@ -1,45 +1,60 @@
 package com.example.deligo.store.controller;
 
+import com.example.deligo.store.dto.request.StoreSaveRequestDto;
 import com.example.deligo.store.dto.request.StoreUpdateRequestDto;
+import com.example.deligo.store.dto.response.StoreResponseDto;
 import com.example.deligo.store.dto.response.StoreSaveResponseDto;
+import com.example.deligo.store.entity.Store;
 import com.example.deligo.store.service.StoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/stores")
 @RequiredArgsConstructor
 public class StoreController {
 
     private final StoreService storeService;
 
-    @PostMapping("/stores")
-    public ResponseEntity<StoreSaveResponseDto> save(@RequestBody StoreUpdateRequestDto dto) {
-        return ResponseEntity.ok(storeService.save(dto));
+    //가게 등록
+    @PostMapping
+    public ResponseEntity<StoreSaveResponseDto> createStore(
+            @RequestParam Long userId,
+            @RequestBody StoreSaveRequestDto dto
+    ) {
+        StoreSaveResponseDto responseDto = storeService.createStore(userId, dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
-    @GetMapping("/stores")
-    public ResponseEntity<List<StoreSaveResponseDto>> findAll() {
-        return ResponseEntity.ok(storeService.findAll());
-    }
+    //모든 가게 조회
+   @GetMapping
+    public ResponseEntity<List<StoreResponseDto>> getAllStore() {
+        List<StoreResponseDto> store = storeService.getAllStore();
+        return ResponseEntity.ok(store);
+   }
 
-    @GetMapping("/stores/{id}")
-    public ResponseEntity<StoreSaveResponseDto> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(storeService.findById(id));
-    }
+   //특정 가게 조회
+   @GetMapping("/{id}")
+    public ResponseEntity<StoreResponseDto> getByStoreId(@PathVariable Long id) {
+        return ResponseEntity.ok(storeService.getByStoreID(id));
+   }
 
-    @PutMapping("/stores/{id}")
-    public ResponseEntity<StoreSaveResponseDto> update(
+   //가게 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<StoreResponseDto> updateStore(
             @PathVariable Long id,
             @RequestBody StoreUpdateRequestDto dto
     ) {
-    return ResponseEntity.ok(storeService.update(id, dto));
+        return ResponseEntity.ok(storeService.updateStore(id, dto));
     }
 
-    @DeleteMapping("/stores/{id}")
-    public void deleteById(Long id) {
-        storeService.deleteById(id);
+    //가게 삭제
+    @DeleteMapping("/{id}")
+    public void deleteByStoreId(Long id) {
+        storeService.deleteByStoreId(id);
     }
 }
--- src/main/java/com/example/deligo/store/service/StoreService.java
+++ src/main/java/com/example/deligo/store/service/StoreService.java
@@ -1,9 +1,13 @@
 package com.example.deligo.store.service;
 
+import com.example.deligo.store.dto.request.StoreSaveRequestDto;
 import com.example.deligo.store.dto.request.StoreUpdateRequestDto;
+import com.example.deligo.store.dto.response.StoreResponseDto;
 import com.example.deligo.store.dto.response.StoreSaveResponseDto;
 import com.example.deligo.store.entity.Store;
 import com.example.deligo.store.repository.StoreRepository;
+import com.example.deligo.user.entity.User;
+import com.example.deligo.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,51 +20,99 @@ import java.util.List;
 public class StoreService {
 
     private final StoreRepository storeRepository;
+    private final UserRepository userRepository;
 
+    //가게 생성
     @Transactional
-    public StoreSaveResponseDto save(StoreUpdateRequestDto dto) {
-        Store store = new Store(dto.getName());
-        Store savestore = storeRepository.save(store);
+    public StoreSaveResponseDto createStore(Long userId, StoreSaveRequestDto dto) {
+        User user = userRepository.findById(userId).orElseThrow(
+                ()-> new IllegalArgumentException("존재하지 않는 사용자입니다")
+        );
+        Store store = Store.builder()
+                .user(user)
+                .name(dto.getName())
+                .category(dto.getStoreCategory())
+                .openTime(dto.getOpenTime())
+                .closeTime(dto.getCloseTime())
+                .minOrderAmount(dto.getMinOrderAmount())
+                .status(dto.getStatus())
+                .build();
+
+        Store savedStore = storeRepository.save(store);
         return new StoreSaveResponseDto(
-                savestore.getId(), savestore.getName(), savestore.getOpenTime(), savestore.getCloseTime(), savestore.getMinOrderAmount(), savestore.getCategory(), savestore.getAverage_rating());
+                savedStore.getId(),
+                savedStore.getOwner().getId(),
+                savedStore.getName(),
+               savedStore.getCategory(),
+                savedStore.getOpenTime(),
+                savedStore.getCloseTime(),
+                savedStore.getMinOrderAmount(),
+                savedStore.getStatus(),
+                savedStore.getAverageRating()
+        );
     }
 
+    //모든 가게 조회
     @Transactional(readOnly = true)
-    public List<StoreSaveResponseDto> findAll() {
+    public List<StoreResponseDto> getAllStore() {
         List<Store> stores = storeRepository.findAll();
-        List<StoreSaveResponseDto> dtos = new ArrayList<>();
-        for(Store store : stores) {
-            dtos.add(new StoreSaveResponseDto(store.getId(), store.getName(), store.getOpenTime(), store.getCloseTime(), store.getMinOrderAmount(), store.getCategory(), store.getAverage_rating()));
-        }
-        return dtos;
+       List<StoreResponseDto> dtos = new ArrayList<>();
+       for (Store store : stores) {
+           dtos.add(new StoreResponseDto(
+                   store.getId(),
+                   store.getOwner().getId(),
+                   store.getName(),
+                   store.getCategory(),
+                   store.getOpenTime(),
+                   store.getCloseTime(),
+                   store.getMinOrderAmount(),
+                   store.getStatus(),
+                   store.getAverageRating()));
+       }
+       return dtos;
     }
 
+    //특정 가게 조회
     @Transactional(readOnly = true)
-    public StoreSaveResponseDto findById(Long id) {
+    public StoreResponseDto getByStoreID(Long id) {
         Store store = storeRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("사용자를 찾을 수 없습니다")
+                ()-> new IllegalArgumentException("가게 id를 찾을 수 없습니다")
         );
-        return new StoreSaveResponseDto(store.getId(), store.getName(), store.getOpenTime(), store.getCloseTime(), store.getMinOrderAmount(), store.getCategory(), store.getAverage_rating());
+        return new StoreResponseDto(
+                store.getId(),
+                store.getOwner().getId(),
+                store.getName(),
+                store.getCategory(),
+                store.getOpenTime(),
+                store.getCloseTime(),
+                store.getMinOrderAmount(),
+                store.getStatus(),
+                store.getAverageRating()
+        );
     }
 
+    //가게 수정
     @Transactional
-    public StoreSaveResponseDto update(Long id, StoreUpdateRequestDto dto) {
+    public StoreResponseDto updateStore(Long id, StoreUpdateRequestDto dto) {
         Store store = storeRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("사용자를 찾을 수 없습니다")
+                ()-> new IllegalArgumentException("가게 id를 찾을 수 없습니다")
         );
-        store.update(
-                dto.getName(),
-                dto.getOpenTime(),
-                dto.getCloseTime(),
-                dto.getMinOrderAmount(),
-                dto.getCategory(),
-                dto.getStatus()
-        );
-        return new StoreSaveResponseDto(store.getId(), store.getName(),store.getOpenTime(), store.getCloseTime(), store.getMinOrderAmount(), store.getCategory(), store.getAverage_rating());
+        store.Update(dto);
+        return new StoreResponseDto(
+                store.getId(),
+                store.getOwner().getId(),
+                store.getName(),
+                store.getCategory(),
+                store.getOpenTime(),
+                store.getCloseTime(),
+                store.getMinOrderAmount(),
+                store.getStatus(),
+                store.getAverageRating());
     }
 
+    //가게 삭제
     @Transactional
-    public void deleteById(Long id) {
+    public void deleteByStoreId(Long id) {
         storeRepository.deleteById(id);
     }
 }
