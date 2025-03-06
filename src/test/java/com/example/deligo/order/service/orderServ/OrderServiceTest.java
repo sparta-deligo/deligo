8d75973001650d107331d7e175d3f33a40e4b306
plusmate <psejin1478@gmail.com>
2025-03-06 20:51:06 +0900

    test: 테스트코드 작성중 오류 발생 커밋

--- src/test/java/com/example/deligo/order/service/orderServ/OrderServiceTest.java
+++ src/test/java/com/example/deligo/order/service/orderServ/OrderServiceTest.java
@@ -4,6 +4,9 @@ import com.example.deligo.menu.entity.Menu;
 import com.example.deligo.menu.entity.MenuStatus;
 import com.example.deligo.menu.repository.MenuRepository;
 import com.example.deligo.order.dto.request.SaveRequest;
+import com.example.deligo.order.dto.response.SaveResponse;
+import com.example.deligo.order.entity.Order;
+import com.example.deligo.order.entity.OrderItem;
 import com.example.deligo.order.repository.OrderItemRepository;
 import com.example.deligo.order.repository.OrderRepository;
 import com.example.deligo.store.entity.Store;
@@ -21,10 +24,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -33,9 +37,9 @@ class OrderServiceTest {
     private OrderServiceImpl orderService;
 
     @Mock
-    private OrderRepository orderRepo;
-    @Mock
     private OrderItemRepository orderItemRepo;
+    @Mock
+    private OrderRepository orderRepo;
     @Mock
     private UserRepository userServ;
     @Mock
@@ -53,18 +57,30 @@ class OrderServiceTest {
         String deliverAddress = "~~시 ~~구 ~~동 ...";
         String storeComment = "리뷰이벤트할게요 새우튀김으로 부탁드려요 :)";
         String riderComment = "천천히 와주세요";
+        Map<Long, Integer> menu_quan = new HashMap<>();
+        menu_quan.put(1L, 2);
+        menu_quan.put(2L, 2);
+
 
         User user = new User("testEmail", "testPw", "testName", UserRole.USER);
         Store store = new Store(user, "testStore", StoreCategory.CHICKEN, LocalTime.now(), LocalTime.now(), 5, StoreStatus.OPEN);
-        SaveRequest saveRequest = new SaveRequest(deliverAddress, menuIds, quantity, riderComment, storeComment, storeId);
-        List<Menu> menus = new ArrayList<>();
-        menus.add(new Menu(store, "testMenu1", "desc", new BigDecimal("10.5"), MenuStatus.AVAILABLE));
-        menus.add(new Menu(store, "testMenu2", "desc", new BigDecimal("10.5"), MenuStatus.AVAILABLE));
+        SaveRequest saveRequest = new SaveRequest(deliverAddress, menu_quan, riderComment, storeComment, storeId);
+//        List<Menu> menus = new ArrayList<>();
+        Menu menu1 = new Menu(store, "testMenu1", "desc", new BigDecimal("10.5"), MenuStatus.AVAILABLE);
+        Menu menu2 = new Menu(store, "testMenu2", "desc", new BigDecimal("10.5"), MenuStatus.AVAILABLE);
+//        menus.add(menu1);
+//        menus.add(menu2);
+
+        given(userServ.findById(any())).willReturn(Optional.of(user));
+        given(storeServ.findById(any())).willReturn(Optional.of(store));
+        given(menuServ.findById(any())).willReturn(Optional.of(menu1));
+        given(menuServ.findById(any())).willReturn(Optional.of(menu2));
 
         /*when*/
-//        SaveResponse saveResponse = orderService.saveOrder(user, store, menus, saveRequest);
+        SaveResponse saveResponse = orderService.saveOrder(userId, saveRequest);
+
 
         /*then*/
-//        assertNotNull(saveResponse);
+        assertNotNull(saveResponse);
     }
 }
