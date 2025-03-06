5333d7c59302b5ae4040f305ed6602274855bc3f
J <kj5592305@gmail.com>
2025-03-06 14:49:10 +0900

    edit

--- src/main/java/com/example/deligo/common/dto/PaginationResponse.java
+++ src/main/java/com/example/deligo/common/dto/PaginationResponse.java
@@ -23,12 +23,8 @@ public class PaginationResponse<T> {
                 .build();
     }
 
-<<<<<<< HEAD
     @Getter // get 메서드 필요
     @Builder
-=======
-    @Getter
->>>>>>> dev
     private static class PaginationInfo {
 
         private final int currentPage;
